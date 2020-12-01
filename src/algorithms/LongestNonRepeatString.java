1f9acb1cf0a3085913ac4f590adfe14edb5fd149
Tim Bucher <tcbucher@gmail.com>
2020-12-01 14:22:14 -0500

    Longest Nonrepeat String
    Created an algorithm based on the Hired assessment to find the longest substring with all unique characters given a string.

---
+++ src/algorithms/LongestNonRepeatString.java
@@ -0,0 +1,45 @@
+package algorithms;
+
+public class LongestNonRepeatString
+{
+    public static void main(String[] args)
+    {
+        String testInput = "sskdjfrngesdfhbikjcgnd";
+
+        String output = findLongestSubString(testInput);
+
+        System.out.println(output);
+    }
+
+
+    private static String findLongestSubString(String input)
+    {
+        String maxChars = "", charsFound;
+
+        for (int start = 0; start < (input.length() - maxChars.length()); start++)
+        {
+            charsFound = "";
+
+            for (String currentChar : input.substring(start).split(""))
+            {
+                // If we find a duplicate, jump the start index
+                // to the character after the first instance of the duplicate, for efficiency
+                // e.g. When it finds the second B in "ABCDEB", will set start to the C
+                if (charsFound.contains(currentChar))
+                {
+                    start = input.indexOf(currentChar, start);
+                    break;
+                }
+
+                charsFound += currentChar;
+            }
+
+            if (charsFound.length() > maxChars.length())
+                maxChars = charsFound;
+
+        }
+
+
+        return maxChars;
+    }
+}
